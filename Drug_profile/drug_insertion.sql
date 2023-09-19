dc47e9960dbd4c3981b5707889c9319a4f7d4205
Ambivalentalfa <aas.nilsson@gmail.com>
2023-09-19 15:10:27 +0200

    Folder of the drug tables and how to populate them

---
+++ Drug_profile/drug_insertion.sql
@@ -0,0 +1,160 @@
+INSERT INTO side_effects (se_name)
+VALUES
+    ('Headache'),
+    ('Nausea'),
+    ('Dizziness'),
+    ('Fatigue'),
+    ('Insomnia'),
+    ('Dry mouth'),
+    ('Constipation'),
+    ('Diarrhea'),
+    ('Vomiting'),
+    ('Blurred vision'),
+    ('Rash'),
+    ('Swelling'),
+    ('Heart palpitations'),
+    ('Anxiety'),
+    ('Depression'),
+    ('Irritability'),
+    ('Weight gain'),
+    ('Weight loss'),
+    ('Loss of appetite'),
+    ('Muscle pain'),
+    ('Joint pain'),
+    ('Back pain'),
+    ('Fever'),
+    ('Chills'),
+    ('Cough'),
+    ('Shortness of breath'),
+    ('Chest pain'),
+    ('Abdominal pain'),
+    ('Indigestion'),
+    ('Gas'),
+    ('Bloating'),
+    ('Frequent urination'),
+    ('Difficulty urinating'),
+    ('Blood in urine'),
+    ('Increased thirst'),
+    ('Frequent infections'),
+    ('Hair loss'),
+    ('Nosebleeds'),
+    ('Tinnitus'),
+    ('Tremors'),
+    ('Difficulty sleeping'),
+    ('Night sweats'),
+    ('Swollen lymph nodes'),
+    ('Sore throat'),
+    ('Cold sores'),
+    ('Canker sores'),
+    ('Numbness'),
+    ('Tingling sensation'),
+    ('Memory problems'),
+    ('Confusion'),
+    ('Hallucinations'),
+    ('Mood swings'),
+    ('Loss of libido'),
+    ('Impotence'),
+    ('Hot flashes'),
+    ('Menstrual irregularities'),
+    ('Swollen ankles'),
+    ('Paleness'),
+    ('Bruising'),
+    ('Chest tightness'),
+    ('Nasal congestion'),
+    ('Runny nose'),
+    ('Sneezing'),
+    ('Itchy eyes'),
+    ('Watery eyes'),
+    ('Dry skin'),
+    ('Itchy skin'),
+    ('Excessive sweating'),
+    ('Muscle spasms'),
+    ('Difficulty concentrating'),
+    ('Dry throat'),
+    ('Heartburn'),
+    ('Jaw pain'),
+    ('Toothache'),
+    ('Chest congestion'),
+    ('Short-term memory loss'),
+    ('Increased appetite'),
+    ('Loss of coordination'),
+    ('Blurry vision'),
+    ('Sensitivity to light'),
+    ('Swollen face'),
+    ('Swollen tongue'),
+    ('Metallic taste in mouth'),
+    ('Unexplained bleeding'),
+    ('Dark urine'),
+    ('Yellowing of skin'),
+    ('Fainting'),
+    ('Loss of balance'),
+    ('Nail discoloration'),
+    ('Swollen fingers'),
+    ('Swollen toes'),
+    ('Frequent hiccups'),
+    ('Stomach cramps'),
+    ('Dry eyes'),
+    ('Loss of smell'),
+    ('Loss of taste'),
+    ('Shortness of temper'),
+    ('Difficulty swallowing'),
+    ('Nervousness'),
+    ('Loss of energy'),
+    ('Loss of strength'),
+    ('Hair thinning'),
+    ('Joint swelling'),
+    ('Muscle weakness'),
+    ('Sensitivity to cold'),
+    ('Sensitivity to heat'),
+    ('Stomach ulcers'),
+    ('Kidney pain'),
+    ('Throat tightness'),
+    ('Weight fluctuations'),
+    ('Bleeding gums'),
+    ('Nasal bleeding'),
+    ('Frequent burping'),
+    ('Facial flushing'),
+    ('Difficulty speaking');
+
+
+
+
+
+INSERT INTO drugs (drug_brand, drug_class, drug_active_ingredient, drug_inactive_ingredient)
+VALUES
+    ('Ortho Tri-Cyclen', 'Combination', 'Norgestimate, Ethinyl Estradiol', 'Lactose, Magnesium Stearate, Microcrystalline Cellulose'),
+    ('Yasmin', 'Combination', 'Drospirenone, Ethinyl Estradiol', 'Lactose, Magnesium Stearate, Corn Starch'),
+    ('Lo Loestrin Fe', 'Combination', 'Norethindrone, Ethinyl Estradiol', 'Lactose, Magnesium Stearate, Croscarmellose Sodium'),
+    ('Alesse', 'Combination', 'Levonorgestrel, Ethinyl Estradiol', 'Lactose, Corn Starch, Magnesium Stearate'),
+    ('Seasonique', 'Combination', 'Levonorgestrel, Ethinyl Estradiol', 'Lactose, Corn Starch, Magnesium Stearate'),
+    ('Micronor', 'Progestin-Only', 'Norethindrone', 'Lactose, Magnesium Stearate, Corn Starch'),
+    ('Norethindrone', 'Progestin-Only', 'Norethindrone', 'Lactose, Magnesium Stearate, Corn Starch'),
+    ('Seasonale', 'Extended-Cycle', 'Levonorgestrel, Ethinyl Estradiol', 'Lactose, Corn Starch, Magnesium Stearate'),
+    ('Lybrel', 'Extended-Cycle', 'Levonorgestrel, Ethinyl Estradiol', 'Lactose, Corn Starch, Magnesium Stearate'),
+    ('Plan B One-Step', 'Emergency Contraceptive', 'Levonorgestrel', 'Lactose, Corn Starch, Magnesium Stearate'),
+    ('Ella', 'Emergency Contraceptive', 'Ulipristal Acetate', 'Lactose, Magnesium Stearate, Corn Starch');
+
+
+INSERT INTO drug_association_ex1 (drug_fk_id, se_fk_id, in_fass, report_count)
+VALUES
+    -- Example associations for drug Ortho Tri-Cyclen
+    (1, 1, 1, 50),  -- Ortho Tri-Cyclen and Headache, in FASS, 50 reports
+    (1, 2, 1, 40),  -- Ortho Tri-Cyclen and Nausea, in FASS, 40 reports
+    (1, 3, 0, 10),  -- Ortho Tri-Cyclen and Dizziness, not in FASS, 10 reports
+
+    -- Example associations for drug Yasmin
+    (2, 1, 1, 30),  -- Yasmin and Headache, in FASS, 30 reports
+    (2, 2, 1, 20),  -- Yasmin and Nausea, in FASS, 20 reports
+    (2, 4, 0, 5),   -- Yasmin and Fatigue, not in FASS, 5 reports
+
+    -- Add more associations for other drugs and side effects here
+    ;
+
+    INSERT INTO drug_association_ex1 (drug_fk_id, se_fk_id, in_fass, report_count)
+VALUES
+    (1, 1, 1, 50),  
+    (1, 2, 1, 40),  
+    (1, 3, 0, 10),
+    (2, 1, 1, 30), 
+    (2, 2, 1, 20), 
+    (2, 4, 0, 5);
