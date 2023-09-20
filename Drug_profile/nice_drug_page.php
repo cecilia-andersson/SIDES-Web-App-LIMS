cb69e194d46e162d25ac2f791756c5f9a060ffce
Ambivalentalfa <aas.nilsson@gmail.com>
2023-09-20 22:23:03 +0200

    Basic colour front end

---
+++ Drug_profile/nice_drug_page.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Drug Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to CSS file -->
+    <style>
+        
+        body {
+            background-color: #ffffff; 
+            font-family: 'Roboto', sans-serif; 
+            font-size: 16px; 
+            color: #1a3038;
+            margin: 20px; 
+        }
+        h1 {
+            color: #256e8a; 
+            margin-bottom: 20px; 
+        }
+        p {
+            margin: 10px 0; 
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Drug Details</h1>
+    <?php
+    // Retrieve the drug information based on the drug_id query parameter
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "things";
+    $drug_id = $_GET['drug_id'];
+
+    // Create connection
+    $link = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (mysqli_connect_error()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Fetch drug details based on drug_id
+    $sql = "SELECT drug_brand, drug_class, drug_active_ingredient, drug_inactive_ingredient
+            FROM drugs
+            WHERE drug_id = $drug_id";
+
+    $result = $link->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        // Display drug information
+        echo "<p>Active Ingredients: " . $row["drug_active_ingredient"] . "</p>";
+        echo "<p>Brand: " . $row["drug_brand"] . "</p>";
+        echo "<p>Class: " . $row["drug_class"] . "</p>";
+        echo "<p>Inactive Ingredients: " . $row["drug_inactive_ingredient"] . "</p>";
+    } else {
+        echo "<p>Drug not found</p>"; // Added formatting for error message
+    }
+
+    // Close the database connection
+    mysqli_close($link);
+    ?>
+</body>
+</html>
---
+++ Drug_profile/nice_search_page.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Drug Search</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to CSS file -->
+    <style>
+       
+        body {
+            background-color: #ffffff; 
+            font-family: 'Roboto', sans-serif; 
+            font-size: 16px; 
+            color: #1a3038; 
+            margin: 20px; 
+        }
+        h1 {
+            color: #256e8a; 
+            margin-bottom: 20px; 
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px; 
+        }
+        th, td {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: left;
+        }
+        a {
+            color: #256e8a;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h1>Drug Search</h1>
+    <form action="search_drugs.php" method="POST">
+        Search: <input type="text" name="search_query">
+        <input type="submit" value="Go">
+    </form>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "things";
+    $search_query = $_POST['search_query']; 
+
+    // Create connection
+    $link = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (mysqli_connect_error()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    echo 'Search results for "' . $search_query . '":';
+    echo '<table><tr><th>Brand</th><th></th><th>Class</th><th></th><th>Active Ingredients</th><th></th><th>Inactive Ingredients</th></tr>';
+
+    $sql = "SELECT drugs.drug_class, drugs.drug_brand, drugs.drug_active_ingredient, drugs.drug_inactive_ingredient, drugs.drug_id
+            FROM drugs
+            WHERE drug_brand LIKE '%$search_query%'
+            OR drug_class LIKE '%$search_query%'
+            OR drug_active_ingredient LIKE '%$search_query%'
+            OR drug_inactive_ingredient LIKE '%$search_query%'";
+
+    $result = $link->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $drug_id = $row["drug_id"];
+            echo "<tr> <td><a href='nice_drug_page.php?drug_id=$drug_id'>" . $row["drug_brand"] . "</a></td><td></td><td>" . $row["drug_class"] . "</td><td></td><td>" . $row["drug_active_ingredient"] . "</td><td></td><td>" . $row["drug_inactive_ingredient"] . "</td></tr>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    // Close the database connection
+    mysqli_close($link);
+    ?>
+
+</body>
+</html>
