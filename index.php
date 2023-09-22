451764ae13c2314c1bf935b6bd1cc586b0d57b8b
Moa Qvarnlöf <moa@qvarnlof.se>
2023-09-22 12:13:39 +0200

    changes in css and navigation

--- index.php
+++ index.php
@@ -1,60 +1,40 @@
-<style>
-.column {
-  float: left;
-  width: 50%;
-}
-
-.row:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-</style>
-
+<!DOCTYPE html>
 <html>
-<link rel="stylesheet" type="text/css" href="stylesheet/styles.css"> <!-- Link to CSS file -->
 
 <head>
-<title>SIDES</title>
-<h3 style="color: #9510AC; display: inline;">SID</h3><h3 style="color: #246F8A; display: inline;">ES</h3>
-<nav style="display: inline;">
-    <a href="index.php">Home</a>
-    <a href="#">Contact</a>
-    <a href="#">About us</a>
-    <a href="#">My profile</a>
-    <a href="#">Forum</a>
-</nav>
+    <title>SIDES</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet/styles.css"> <!-- Link to CSS file -->
+    <h3 style="color: #9510AC; display: inline;">SID</h3><h3 style="color: #246F8A; display: inline;">ES</h3>
+    <nav style="display: inline;">
+        <a href="Drug_profile/nice_search_page.php">Search</a>
+        <a href="index.php">Home</a>
+        <a href="#">Contact</a>
+        <a href="#">About us</a>
+        <a href="#">My profile</a>
+        <a href="#">Forum</a>
+    </nav>
+    <form action="user_profile/login_page.php" style="display: inline">
+        <input type="submit" value="Log in">
+    </form>
+    <form action="user_profile/register.php" style="display: inline">
+        <input type="submit" value="Register">
+    </form>
 </head>
+
 <body>
-<h1 style="color: 1A3038; margin-bottom: 3%;"> Real people </h1>
-<h1 style="color: 9510AC; margin-top: 3%;">Real side effects </h1> 
+<?php
+if (isset($_GET['Message'])) {
+    echo $_GET['Message'];
+}
+?>
 
-<p style="color: 757CB3;"> We keep track of how your medications affect you. Share experiences anonymously with others to collectively feel better. </p>
+    <h1 style="color: #1A3038; margin-bottom: 3%;"> Real people </h1>
+    <h1 style="color: #9510AC; margin-top: 3%;">Real side effects </h1>
 
-<div class="row">
-    <div class="column">
-        <h3> Log in </h3>
-        <p>
-            <form action="user_profile/login_page.php">
-                <input type="submit" value="Login"> <br>
-            </form>
+    <p style="color: #757CB3;"> We keep track of how your medications affect you. Share experiences anonymously with
+        others to collectively feel better. </p>
 
-            <?php
-            if(isset($_GET['Message'])){
-                echo $_GET['Message'];
-            }
-            ?>
-        </p>
-    </div>
 
-    <div class="column">
-        <h3> New here? Register an account today! </h3>
-        <p>
-            <form action="user_profile/register.php">
-                <input type="submit" value="Register">
-            </form>
-        </p>
-    </div>
-</div>
 </body>
+
 </html>
--- user_profile/register.php
+++ user_profile/register.php
@@ -5,32 +5,45 @@
     <title>Register</title>
     <link rel="stylesheet" type="text/css" href="../stylesheet/styles.css"> <!-- Link to CSS file -->
     <style>
-        body{
+        body {}
 
-        }
-
-        .register-button{
-
-        }
-    
+        .register-button {}
     </style>
+    <h3 style="color: #9510AC; display: inline;">SID</h3><h3 style="color: #246F8A; display: inline;">ES</h3>
+    <nav style="display: inline;">
+        <a href="../Drug_profile/nice_search_page.php">Search</a>
+        <a href="../index.php">Home</a>
+        <a href="#">Contact</a>
+        <a href="#">About us</a>
+        <a href="#">My profile</a>
+        <a href="#">Forum</a>
+    </nav>
+    <form action="login_page.php" style="display: inline">
+        <input type="submit" value="Log in">
+    </form>
+    <form action="register.php" style="display: inline">
+        <input type="submit" value="Register">
+    </form>
 </head>
 <h2> Create your SIDES account right now baby </h2>
 
 
 <body>
-<!-- POST / PUT? Urlencode? -->
-<form action="insertdata.php" method="POST">
-    Username: <input type="text" name="username" required> <br>
-    Password: <input type="password" name="pwd" pattern="^(?=.*[A-Z])(?=.*\d).+$" title="Password must contain at least one capital letter and one number." required> <br>
-    Personal Identity Number: <input type="text" name="personnmr" placeholder="YYYYMMDDXXXX" pattern="(19[0-9]{2}|20[0-1][0-5])[0-9]{8}" required> <br>
-    <input type="submit" value="Add" class="register-button"><br>
+    <!-- POST / PUT? Urlencode? -->
+    <form action="insertdata.php" method="POST">
+        Username: <input type="text" name="username" required> <br>
+        Password: <input type="password" name="pwd" pattern="^(?=.*[A-Z])(?=.*\d).+$"
+            title="Password must contain at least one capital letter and one number." required> <br>
+        Personal Identity Number: <input type="text" name="personnmr" placeholder="YYYYMMDDXXXX"
+            pattern="(19[0-9]{2}|20[0-1][0-5])[0-9]{8}" required> <br>
+        <input type="submit" value="Add" class="register-button"><br>
 
-</form>
-<?php
-    if(isset($_GET['Message'])){
+    </form>
+    <?php
+    if (isset($_GET['Message'])) {
         echo $_GET['Message'];
     }
     ?>
 </body>
+
 </html>
