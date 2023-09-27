2d9aac6b83ed9ef3b7ff6e313f3ad5225f929261
Moa Qvarnlöf <moa@qvarnlof.se>
2023-09-27 18:40:28 +0200

    changes in nav

--- index.php
+++ index.php
@@ -9,15 +9,14 @@
 <body>
     <header>
         <link rel="stylesheet" type="text/css" href="stylesheet/styles.css"> <!-- Link to CSS file -->
-        <h3 style="color: #9510AC; display: inline;">SID</h3><h3 style="color: #246F8A; display: inline; margin-right: 2em;">ES</h3>
-        <nav style="display: inline;">
+        <nav>
+            <a href="index.php">
+                <img src="images/SIDES_head.png" alt="Home" style="width: 15px;">
+                <h3 style="color: #9510AC; display: inline;">SID</h3><h3 style="color: #246F8A; display: inline;">ES</h3>
+            </a>
             <a href="Drug_profile/nice_search_page.php" style="text-decoration: none;">
                 <img src="images/search.png" alt="Search Drugs" style="width: 15px;">
             </a>
-            <!-- <a href="Drug_profile/nice_search_page.php">Search</a> -->
-            <a href="index.php">Home</a>
-            <a href="user_profile/contact.php">Contact</a>
-            <a href="user_profile/aboutus.php">About us</a>
             <a href="user_profile/forum.php">Forum</a>
             <?php
             session_start();
