8bc796666db8e5d2a943435deba1fd3bd40baf2b
Moa Qvarnlöf <moa@qvarnlof.se>
2023-10-07 17:00:43 +0200

    Update myprofile.php

--- user_profile/myprofile.php
+++ user_profile/myprofile.php
@@ -16,7 +16,7 @@ include "../footer.php";
             padding: 1rem;
             text-align: left;
             max-width: fit-content;
-            max-height:210px;
+            max-height: 210px;
         }
 
         p {
@@ -36,7 +36,8 @@ include "../footer.php";
         .contraceptives {
             position: relative;
         }
-        main{
+
+        .all_content {
             display: flex;
             justify-content: space-between;
         }
@@ -53,107 +54,112 @@ include "../footer.php";
 
     <h2>Your profile</h2>
     <main>
-        <?php
-        if (isset($_SESSION['username'])) {
-            $loggedInUser = $_SESSION['username'];
-            $personalnumber = $_SESSION['personalnumber'];
-            $yearnow = date("Y");
-            $useryear = substr($personalnumber, 0, 4);
-            $age = $yearnow - $useryear;
+        <div class="all_content"> <!-- All content -->
+            <?php
+            if (isset($_SESSION['username'])) {
+                $loggedInUser = $_SESSION['username'];
+                $personalnumber = $_SESSION['personalnumber'];
+                $yearnow = date("Y");
+                $useryear = substr($personalnumber, 0, 4);
+                $age = $yearnow - $useryear;
 
-            if (substr($age, 1, 2) > 5) {
-                $agerange = substr($age, 0, 1) . "6" . "-" . substr(($age + 1), 0, 1) . "0";
-            } else {
-                $agerange = substr($age, 0, 1) . "0" . "-" . substr(($age + 1), 0, 1) . "5";
-            }
-            $sql = "SELECT * FROM users WHERE username = '$loggedInUser'";
-            $result = $link->query($sql);
-
-            if ($result->num_rows == 1) {
-                $row = $result->fetch_assoc();
-                $email = $row['email'];
-            }
-            ?>
-
-            <div class='contraceptives'>
-                <h4>Current contraceptives</h4>
-                <?php
-                $sql = "SELECT userid FROM users WHERE username = '$loggedInUser'";
+                if (substr($age, 1, 2) > 5) {
+                    $agerange = substr($age, 0, 1) . "6" . "-" . substr(($age + 1), 0, 1) . "0";
+                } else {
+                    $agerange = substr($age, 0, 1) . "0" . "-" . substr(($age + 1), 0, 1) . "5";
+                }
+                $sql = "SELECT * FROM users WHERE username = '$loggedInUser'";
                 $result = $link->query($sql);
+
                 if ($result->num_rows == 1) {
                     $row = $result->fetch_assoc();
-                    $userid = $row["userid"];
+                    $email = $row['email'];
                 }
-
-                $sql = "SELECT * FROM user_drug WHERE userid = $userid";
-                $result = $link->query($sql);
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        $drug_id = $row["drug_id"];
-                        $date = $row["reg_date"];
-                        $Date = date("Y-m-d", strtotime($date));
-                        $sql1 = "SELECT drug_brand FROM drugs WHERE drug_id = $drug_id";
-                        $result1 = $link->query($sql1);
-                        if ($result1->num_rows > 0) {
-                            $row = $result1->fetch_assoc();
-                            $drug_brand = $row["drug_brand"];
-                            echo "<p><a href='../Drug_profile/nice_drug_page.php?drug_id=$drug_id'>$drug_brand</a> since $Date</p>";
-                        } else {
-                            echo "<p>Drug_id does not exist in the drug table</p>";
+                ?>
+                <div class="left_content"> <!-- Left content -->
+                    <div class='contraceptives'> <!-- Contraceptives -->
+                        <h4>Current contraceptives</h4>
+                        <?php
+                        $sql = "SELECT userid FROM users WHERE username = '$loggedInUser'";
+                        $result = $link->query($sql);
+                        if ($result->num_rows == 1) {
+                            $row = $result->fetch_assoc();
+                            $userid = $row["userid"];
                         }
-                    }
-                } else {
-                    echo "<p>No reported contraceptives</p>";
-                }
-                echo "</div>";
 
-
-
-                include "calendar.php";
-                echo "<section>
+                        $sql = "SELECT * FROM user_drug WHERE userid = $userid";
+                        $result = $link->query($sql);
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $drug_id = $row["drug_id"];
+                                $date = $row["reg_date"];
+                                $Date = date("Y-m-d", strtotime($date));
+                                $sql1 = "SELECT drug_brand FROM drugs WHERE drug_id = $drug_id";
+                                $result1 = $link->query($sql1);
+                                if ($result1->num_rows > 0) {
+                                    $row = $result1->fetch_assoc();
+                                    $drug_brand = $row["drug_brand"];
+                                    echo "<p><a href='../Drug_profile/nice_drug_page.php?drug_id=$drug_id'>$drug_brand</a> since $Date</p>";
+                                } else {
+                                    echo "<p>Drug_id does not exist in the drug table</p>";
+                                }
+                            }
+                        } else {
+                            echo "<p>No reported contraceptives</p>";
+                        }
+                        ?>
+                    </div> <!-- Contraceptives -->
+                    <?php
+                    // PROFILE INFO
+                    echo "<br><section>
         <p> <h4> About you </h4> <p>
         <p> <b> Username </b> <br> $loggedInUser </p>
         <p> <b> Age </b> <br> $agerange </p>
         <p> <b> Email </b> <br> $email </p>
         "; // this cannot be displayed if salted and hashed
-        
-        }
-        ?>
-            <form action="edit_myprofile.php">
-                <input type="submit" value="Edit profile" style="background-color:#1A3038" />
-            </form>
+            
+            }
+            ?>
+                <form action="edit_myprofile.php">
+                    <input type="submit" value="Edit info" style="background-color:#1A3038" />
+                </form>
+                </section> <!--Profile info-->
+            </div><!-- Left content -->
 
-            </section>
 
+            <?php
+            include "calendar.php";
+            ?>
 
+            <div class="forms"><!--Forms-->
+                <br>
+                <p>
+                    <strong>Something doesn't look right?</strong> <br>
+                <form action="../Forms/changedrug_form.php" method="POST">
+                    <input type="submit" value="Update my contraceptive">
+                </form>
+                <p>
+                <p>
+                <form action="../Forms/changesides_form.php" method="POST">
+                    <input type="submit" value="Change my top SIDES">
+                </form>
+                <br>
+                </p>
+                <p>
+                    <strong> More options: </strong>
 
+                <form action="../Forms/rating_form.php" method="POST">
+                    <input type="submit" value="Review a drug">
+                </form>
+                </p>
+                <p>
+                <form action="../Forms/sidesreport_form.php" method="POST">
+                    <input type="submit" value="Report Past SIDES">
+                </form>
+                </p>
+            </div><!-- Forms -->
     </main>
-
-<p>
-<br> 
-<form action="../Forms/changedrug_form.php" method="POST">
-    <strong>Something doesn't look right?</strong> <br>
-    <input type="submit" value="Update my contraceptive">
-</form>
-<br>
-<form action="../Forms/changesides_form.php" method="POST">
-    <input type="submit" value="Change my top SIDES">
-</form>
-<br>
-<strong> More options: </strong>
-<br>
-<form action="../Forms/rating_form.php" method="POST">
-    <input type="submit" value="Review a drug">
-</form>
-<br>
-<form action="../Forms/sidesreport_form.php" method="POST">
-    <input type="submit" value="Report Past SIDES">
-</form>
-
-</p>
-
-
+    </div><!-- All content -->
 </body>
-
 
 </html>
