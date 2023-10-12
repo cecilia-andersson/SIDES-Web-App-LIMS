6f7c23bfa5a6bf5b742b78b112bcfb60a3dadb7b
elvirazetterberg <elvira.zetterberg@gmail.com>
2023-10-12 14:18:46 +0200

    Added framework for GDPR privacy policy. Also added to footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
     <a href="../user_profile/contact.php">Contact</a>
     <a href="../user_profile/aboutus.php">About us</a>
+    <a href="../user_profile/privacypolicy.php">Privacy policy</a>
 </footer>
