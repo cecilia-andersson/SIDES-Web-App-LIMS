d9d17ec03773a98c078ae80329ee02e9ad4c43c0
Moa Qvarnlöf <moa@qvarnlof.se>
2023-10-13 13:44:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
     <footer>
         <a href="user_profile/contact.php">Contact</a>
         <a href="user_profile/aboutus.php">About us</a>
-        <a href="user_profile/privacypolicy.php">About us</a>
+        <a href="user_profile/privacypolicy.php">Privacy policy</a>
     </footer>
 
 </body>
