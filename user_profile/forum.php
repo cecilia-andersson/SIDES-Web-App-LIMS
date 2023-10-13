169e4fa4bd351373b87cb80d8af4177d8a899093
Moa Qvarnlöf <moa@qvarnlof.se>
2023-10-13 14:36:25 +0200

    Update forum.php

--- user_profile/forum.php
+++ user_profile/forum.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-    
+
 <head>
     <title>Forum</title>
     <link href="../images/SIDES_head_icon.png" rel="icon">
     <style>
-        .forum-post{
+        .forum-post {
             border: 5px solid #256e8a;
             padding: 5px;
             margin-bottom: 5px;
@@ -13,69 +13,65 @@
             justify-content: space-between;
             align-items: flex-start;
         }
-        </style>
+    </style>
 </head>
+
 <body>
-<header>
+    <header>
+        <?php
+        include "../DB_connect.php";
+        include "../navigation.php";
+        ?>
+    </header>
+
+
     <?php
-    include "../DB_connect.php";
-    include "../navigation.php";
-    ?>
-</header>
-
-
-<?php
     include "../footer.php";
-?>
+    ?>
 
+    <?php
 
-
-
-<?php
-
-
-session_start();
-echo '<div style = "text-align:center;">';
-echo '<h1  a </h1>';
-echo '<form method="GET" action="searchpost.php">';
-echo    '<input type="text" name="keyword", placeholder="Search for a username or a drug name" required>';
-echo    '<input type="submit" value="Search">';
-echo '<br>';
-echo '</form>';
-echo '</div>';
-
-if (isset($_SESSION['username']) && isset($_SESSION["id"])) {
-    echo 'Submit your post here:';
-
-    echo '<form action="insertpost.php" method="POST">';
-    echo '<p>';
-    echo '<input type="text" name="post_text" placeholder="Write here:" required>';
-    echo '</p>';
-    echo '<input type="submit" value="Submit"><br>';
+    echo '<div style = "text-align:center;">';
+    echo '<h1  a </h1>';
+    echo '<form method="GET" action="searchpost.php">';
+    echo '<input type="text" name="keyword", placeholder="Search for a username or a drug name" required>';
+    echo '<input type="submit" value="Search">';
     echo '<br>';
     echo '</form>';
+    echo '</div>';
 
-}
+    if (isset($_SESSION['username']) && isset($_SESSION["id"])) {
+        echo 'Submit your post here:';
 
-?>
+        echo '<form action="insertpost.php" method="POST">';
+        echo '<p>';
+        echo '<input type="text" name="post_text" placeholder="Write here:" required>';
+        echo '</p>';
+        echo '<input type="submit" value="Submit"><br>';
+        echo '<br>';
+        echo '</form>';
+
+    }
+
+    ?>
 
 
-<?php
+    <?php
 
-$sql = "SELECT forum_posts.*, users.username, drugs.drug_brand
+    $sql = "SELECT forum_posts.*, users.username, drugs.drug_brand
         FROM forum_posts
         INNER JOIN users ON forum_posts.userid = users.userid
         INNER JOIN user_drug ON forum_posts.user_drug_id = user_drug.user_drug_id
         INNER JOIN drugs ON user_drug.drug_id = drugs.drug_id
         ORDER BY forum_posts.post_date DESC";
 
-$result = $link->query($sql);
+    $result = $link->query($sql);
 
-if($result->num_rows > 0){
-    while ($row = $result->fetch_assoc()){
-        $post_likes = $row['post_likes'];
-        $postID = $row['post_id'];
-        $userID = $row['userid'];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $post_likes = $row['post_likes'];
+            $postID = $row['post_id'];
+            $userID = $row['userid'];
             echo '<a href ="post_w_comments.php?postID=' . $postID . '">';
             echo '<div class="forum-post">';
             echo '<div>';
@@ -83,7 +79,7 @@ if($result->num_rows > 0){
             echo '<p><strong>Post Text:</strong> ' . $row['post_text'] . '</p>';
             echo '</div>';
             echo '<div>';
-            echo '<p><strong>Drug:</strong> ' .  $row['drug_brand'] . '</p>';
+            echo '<p><strong>Drug:</strong> ' . $row['drug_brand'] . '</p>';
             echo '</div>';
             echo '<p><strong>Post Date:</strong> ' . $row['post_date'] . '</p>';
             echo '</div>';
@@ -93,14 +89,14 @@ if($result->num_rows > 0){
             echo '<form action="/LIMS-Flubber/user_profile/likes_post.php" method="POST">'; /*?postID=' . $postID . '*/
             echo '<input type="hidden" name="postID" value="' . $postID . '">';
             echo '<input type="hidden" name="userID" value="' . $userID . '">';
-            echo '<input type="submit" value="&#x1F44D" />' . " " . $post_likes . " " . "likes" . '' ;
+            echo '<input type="submit" value="&#x1F44D" />' . " " . $post_likes . " " . "likes" . '';
             echo '</form>';
             /*echo '<a href="/LIMS-Flubber/likes.php?postID=' . $postID . '">&#x1F44D; Like</a>';*/
 
+        }
     }
-}
 
-?>
+    ?>
 </body>
-</html>
 
+</html>
