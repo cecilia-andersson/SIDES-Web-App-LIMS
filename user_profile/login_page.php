1d2a55c1b07decdb1898bbe3514cf11af2068f22
elvirazetterberg <elvira.zetterberg@gmail.com>
2023-10-22 15:37:54 +0200

    Added my slides

--- user_profile/login_page.php
+++ user_profile/login_page.php
@@ -4,6 +4,82 @@
 <head>
     <title>Login</title>
     <link href="../images/SIDES_head_icon.png" rel="icon">
+    <style>
+                /* START PRESENTATION SLIDES */
+        /* Start column division */
+        .column {
+        float: left;
+        width: 50%;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+        /* End column division */
+
+        .slides_button {
+            background-color: #9510AC;
+            border: none;
+            color: white;
+            position: absolute; 
+            top: 40%;
+            border-radius: 50%;
+            padding: 25px;
+            width: 100px;
+            height: 100px;
+        }
+
+        /* Start slide overlay */
+        #overlay {
+        position: fixed;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color:  rgba(0,0,10, 0.5); /*this is 757CB3 */
+        z-index: 2;
+        cursor: pointer;
+        }
+
+        #overlay2 {
+        position: fixed;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color:  rgba(0,0,10, 0.5); /*this is 757CB3 */
+        z-index: 2;
+        cursor: pointer;
+        }
+
+        #outerContainer{
+        background-color: #ffffff; 
+        border: 2px solid #256e8a;
+        border-radius: 15px;
+        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+
+        max-height: 95vh; /* Set maximum height for the container */
+        overflow-y: auto; /* Enable vertical scrolling if content overflows */
+    
+        position: absolute;
+        top: 50%;
+        left: 50%;
+
+        transform: translate(-50%,-50%);
+        -ms-transform: translate(-50%,-50%);
+        }
+        /* End slide overlay */
+        /* END PRESENTATION SLIDES */
+    </style>
 </head>
 
 <body>
@@ -12,26 +88,87 @@
         include "../navigation.php";
         ?>
     </header>
-    <h2> Sign in </h2>
-    <p>
-    <form action="login.php" method="POST">
-        <p>
-            <input type="text" name="username" placeholder="Username" required> <br>
-        </p>
-        <p>
-            <input type="password" name="login_password" placeholder="Password" required> <br>
-        </p>
-        <input type="submit" value="Sign in"> <br>
-    </form>
-    <p>
-        <a href="forgot_pwd.php">I forgot my password</a> <br><br>
-    </p>
-    <?php
-    if (isset($_GET['Message'])) {
-        echo $_GET['Message'];
-    }
-    ?>
-    </p>
+    <div class="row">
+        <div class="column">
+            <h2> Sign in </h2>
+            <p>
+            <form action="login.php" method="POST">
+                <p>
+                    <input type="text" name="username" placeholder="Username" required> <br>
+                </p>
+                <p>
+                    <input type="password" name="login_password" placeholder="Password" required> <br>
+                </p>
+                <input type="submit" value="Sign in"> <br>
+            </form>
+            <p>
+                <a href="forgot_pwd.php">I forgot my password</a> <br><br>
+            </p>
+            <?php
+            if (isset($_GET['Message'])) {
+                echo $_GET['Message'];
+            }
+            ?>
+            </p>
+        </div>
+        <div class="column">
+            <div id="overlay">
+                <div id="outerContainer">
+                    <h4> Registering an account </h4>
+                    <ul>
+                        <li> Password verified against the stored salted and hashed one connected to the username </li>
+                        <li> Block user 5 failed login attempts </li>
+                        <ul> 
+                            <li> Blocked IP address </li>
+                            <li> Blocked for 1 hour </li>
+                            <li> Message with time left </li>
+                        </ul>
+                        <li> Sent to My profile-page </li>
+                        <li> User ID, birthdate and username are stored in sessions variables after sign in </li>                
+                    </ul>
+                </div>
+            </div>
+            <div id="overlay2">
+                <div id="outerContainer">
+                    <h4> Resetting password </h4>
+                    <ul>
+                        <li> Email with token in link sent to user </li>
+                        <li> Token is a string of 16 random bytes → hexadecimal </li> 
+                        <li> Token, email and expiry stored in temporary table </li> 
+                        <li> Token deleted after use or renewed if reset is requested again </li>
+                    </ul>
+                </div>
+            </div>
+            <button type="button" class="slides_button" onclick="overlay_on()">SLIDES</button>
+            <button type="button" class="slides_button" style="right:30%" onclick="overlay2_on()">SLIDES2</button>
+        </div>
+    </div>
+    <script>
+        function overlay_on() {
+        document.getElementById("overlay").style.display = "block";
+        }
+
+        function overlay_off() {
+        document.getElementById("overlay").style.display = "none";
+        }
+
+        function overlay2_on() {
+        document.getElementById("overlay2").style.display = "block";
+        }
+
+        function overlay2_off() {
+        document.getElementById("overlay2").style.display = "none";
+        }
+
+        document.addEventListener("keydown", function(event) {// to allow for esc closing 
+        if (event.key === "Escape") {
+            overlay_off();
+            overlay2_off(); y
+        }
+        });
+
+    </script>
+
     <?php
     include "../footer.php";
     ?>
--- user_profile/register.php
+++ user_profile/register.php
@@ -8,6 +8,67 @@
         body {}
 
         .register-button {}
+
+        /* START PRESENTATION SLIDES */
+        /* Start column division */
+        .column {
+        float: left;
+        width: 50%;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+        /* End column division */
+
+        .slides_button {
+            background-color: #9510AC;
+            border: none;
+            color: white;
+            position: absolute; 
+            top: 40%;
+            border-radius: 50%;
+            padding: 25px;
+            width: 100px;
+            height: 100px;
+        }
+
+        /* Start slide overlay */
+        #overlay {
+        position: fixed;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color:  rgba(0,0,10, 0.5); /*this is 757CB3 */
+        z-index: 2;
+        cursor: pointer;
+        }
+
+        #outerContainer{
+        background-color: #ffffff; 
+        border: 2px solid #256e8a;
+        border-radius: 15px;
+        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+
+        max-height: 95vh; /* Set maximum height for the container */
+        overflow-y: auto; /* Enable vertical scrolling if content overflows */
+    
+        position: absolute;
+        top: 50%;
+        left: 50%;
+
+        transform: translate(-50%,-50%);
+        -ms-transform: translate(-50%,-50%);
+        }
+        /* End slide overlay */
+        /* END PRESENTATION SLIDES */
     </style>
 </head>
 
@@ -18,35 +79,82 @@
         include "../navigation.php";
         ?>
     </header>
-    <h2> Register </h2>
+    <!-- row div and column div are for presentation -->
+    <div class="row">
+        <div class="column">
+            <h2> Register </h2>
+            
+            <form action="insertdata.php" method="POST">
+                <p>
+                    <input type="text" name="username" placeholder="Username" required>
+                </p>
+                <p>
+                    <input type="password" name="pwd" pattern="^(?=.*[A-Z])(?=.*\d).+$"
+                        title="Password must contain at least one capital letter and one number." placeholder="Password"
+                        required>
+                </p>
+                <p>
+                    <input type="text" name="personnmr" placeholder="Personal Identity Number (YYYYMMDDXXXX)"
+                        pattern="(19[0-9]{2}|20[0-9][0-9])[0-9]{8}" required>
+                </p>
+                <p>
+                    <input size="50" type="email" name="email" placeholder="E-mail (example@email.com)" required>
+                </p>
+                <?php echo 'I have read and accept SIDES <a href="http://localhost:443/user_profile/privacypolicy.php">privacy policy</a>.';?>
+                <p>
+                    <input type="checkbox" name="acceptGDPR" value="I accept" required> 
+                </p>
+                <input type="submit" value="Submit"><br>
+            </form>
+            <?php
+            if (isset($_GET['Message'])) {
+                echo $_GET['Message'];
+            }
+            ?>
+        </div>
+        <div class="column">
+            <div id="overlay">
+                <div id="outerContainer">
+                    <h4> Registering an account </h4>
+                    <ul>
+                        <li> Username </li>
+                        <li> Password (>=1 capital letter, >=1 number, >= 8 characters) </li>
+                        <ul> 
+                            <li> Hashing is one-way </li>
+                            <li> Salting </li>
+                            <li> Built-in php function and bcrypt algorithm </li>
+                        </ul>
+                        <li> Personal security number</li>
+                        <ul> 
+                            <li> birthdate → get age of user </li>
+                            <li> Verify that the user is unique </li> 
+                            <li> Can't use BankID </li> 
+                        </ul>
+                        <li> Email address (unique) </li>
+                        <!-- Say this here?? -->
+                        <li> Account and all associated data can be deleted at any point by a user </li>
+                    </ul>
+                </div>
+            </div>
+            <button type="button" class="slides_button" onclick="overlay_on()">SLIDES</button>
+        </div>
+    </div>
+    <script>
+        function overlay_on() {
+        document.getElementById("overlay").style.display = "block";
+        }
 
-    <form action="insertdata.php" method="POST">
-        <p>
-            <input type="text" name="username" placeholder="Username" required>
-        </p>
-        <p>
-            <input type="password" name="pwd" pattern="^((?=.*[A-Z])(?=.*\d).+)([a-zA-Z1-9]){7,}$"
-                title="Password must contain at least one capital letter and one number." placeholder="Password"
-                required>
-        </p>
-        <p>
-            <input type="text" name="personnmr" placeholder="Personal Identity Number (YYYYMMDDXXXX)"
-                pattern="(19[0-9]{2}|20[0-9][0-9])[0-9]{8}" required>
-        </p>
-        <p>
-            <input size="50" type="email" name="email" placeholder="E-mail (example@email.com)" required>
-        </p>
-        <?php echo 'I have read and accept SIDES <a href="http://localhost:443/user_profile/privacypolicy.php">privacy policy</a>.';?>
-        <p>
-            <input type="checkbox" name="acceptGDPR" value="I accept" required> 
-        </p>
-        <input type="submit" value="Submit"><br>
-    </form>
-    <?php
-    if (isset($_GET['Message'])) {
-        echo $_GET['Message'];
-    }
-    ?>
+        function overlay_off() {
+        document.getElementById("overlay").style.display = "none";
+        }
+
+        document.addEventListener("keydown", function(event) {// to allow for esc closing 
+        if (event.key === "Escape") {
+            overlay_off(); y
+        }
+        });
+    </script>
+
     <?php
     include "../footer.php";
     ?>
